5618a0ac7e2e66376418909c4da9259ae4550884
Orlan03 <or504553@gmail.com>
2025-03-25 22:12:39 -0500

    actualizacion

--- sistema/aplicaciones/carpetas/urls.py
+++ sistema/aplicaciones/carpetas/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('crear/', views.crear_carpeta, name='crear_carpeta'),
     path('ver/<int:carpeta_id>/', views.ver_carpeta, name='ver_carpeta'),
     path('eliminar/<int:carpeta_id>/', views.eliminar_carpeta, name='eliminar_carpeta'),
-    path('subir/<int:carpeta_id>/', views.subir_documento, name='subir_documento'),  # 🔹 Debe recibir 'carpeta_id'
+    path('subir/<int:carpeta_id>/', views.subir_documento, name='subir_documento'),  
     path('crear-subcarpeta/<int:carpeta_id>/', views.crear_subcarpeta, name='crear_subcarpeta'),
 ]
